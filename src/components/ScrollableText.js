76b9d374b518b680e1c015bfe69e598216569cb1
Heyamn <aman2000tailor29@gmail.com>
2023-01-21 23:33:42 +0530

    first commit

---
+++ src/components/ScrollableText.js
@@ -0,0 +1,57 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
+import { connect } from "react-redux";
+import getText from "../actions/text";
+
+const ScrollableText = ({ getText, texts }) => {
+  useEffect(() => {
+    getText();
+  }, [getText]);
+
+  return (
+    <div className="content" style={{ width: "45%" }}>
+      {texts.map((text, index) => (
+        <div className="container" id="panel1" key={index}>
+          <div style={{ backgroundColor: "White", width: "100%" }}>
+            <Card className="mb-2 border-0">
+              <Card.Body>
+                <Card.Title style={{ fontSize: "2rem" }}>
+                  {text.heading}
+                </Card.Title>
+                <Card.Text
+                  style={{
+                    fontWeight: "600",
+                    fontSize: "4.5rem",
+                  }}
+                >
+                  {text.subHeading}
+                </Card.Text>
+
+                <Card.Text
+                  style={{
+                    fontSize: "1.5rem",
+                    marginTop: "10%",
+                  }}
+                >
+                  {text.description}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+ScrollableText.propTypes = {
+  getText: PropTypes.func.isRequired,
+  texts: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  texts: state.text,
+});
+export default connect(mapStateToProps, { getText })(ScrollableText);
